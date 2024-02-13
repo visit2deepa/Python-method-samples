b07e9f3f42f8d78a38b37ad203900a08ee263524
visit2deepa <159101256+visit2deepa@users.noreply.github.com>
2024-02-13 22:37:58 +0800

    test.py

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("hello")
 hello
+>>>for i in range(1,4):
+       print(i*'#')
+#
+##
+###
